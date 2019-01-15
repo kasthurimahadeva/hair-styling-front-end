bbc952190744f505838888a14dff43d5c54d9019
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-01-15 12:40:16 +0530

    login component created

---
+++ src/app/main/app/components/login/login.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FuseConfigService } from '@fuse/services/config.service';
+import { fuseAnimations } from '@fuse/animations/index';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
+})
+export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    returnUrl: string;
+
+    constructor(
+        private _fuseConfigService: FuseConfigService,
+        private _formBuilder: FormBuilder,
+        private router: Router,
+        private route: ActivatedRoute,
+        // private authService: AuthenticationService
+    ) {
+        // Configure the layout
+        this.hideComponents();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        // this.authService.logout();
+
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+        this.hideComponents();
+
+        this.loginForm = this._formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    private hideComponents(): void {
+        this._fuseConfigService.config = {
+            layout: {
+                navbar: {
+                    hidden: true
+                },
+                toolbar: {
+                    hidden: true
+                },
+                footer: {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
+
+    login() {
+        // this.authService
+        //     .authenticate({
+        //             username: this.loginForm.controls['userName'].value,
+        //             password: this.loginForm.controls['password'].value
+        //         }
+        //         , () => {
+        //             console.log('logged in successfully');
+        //             this.router.navigate(['dashboard']);
+        //         });
+        // return false;
+    }
+}
