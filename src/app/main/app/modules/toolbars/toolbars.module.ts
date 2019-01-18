e18026fefd3e30762eedce46ca8f71246f7393f4
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-01-18 07:20:23 +0530

    major changes

--- src/app/main/app/modules/toolbars/toolbars.module.ts
+++ src/app/main/app/modules/toolbars/toolbars.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonToolbarComponent } from './common-toolbar/common-toolbar.component';
-import {MatToolbarModule } from '@angular/material';
+import {MatToolbarModule, MatButtonModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule
   ],
   declarations: [
       CommonToolbarComponent
