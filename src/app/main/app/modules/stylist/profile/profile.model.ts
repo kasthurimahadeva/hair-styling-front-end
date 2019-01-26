81c1fd57885654f04aa40d24bcdefb49b4ad93ae
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-01-26 15:29:07 +0530

    profile model added

---
+++ src/app/main/app/modules/stylist/profile/profile.model.ts
@@ -0,0 +1,18 @@
+export class Profile {
+    id: String;
+    firstName: String;
+    lastName: String;
+    description: String;
+    email: String;
+    telephoneNo: String;
+    country: String;
+    state: String;
+    city: String;
+    street: String;
+    skills: Array<String>;
+    rate: String;
+    bankName: String;
+    branch: String;
+    name: String;
+    accountNo: Number;
+}
